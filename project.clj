d296de4753fe44a08a5eaacec514fa7fd14c6b27
Bryan Maass <bryan.maass@aclaimant.com>
2021-02-03 09:48:04 -0600

    bump version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject escherize/historia "0.1.1"
+(defproject escherize/historia "0.1.2"
   :description "Record and observe what your functions are doing"
   :url "https://github.com/escherize/historia"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
@@ -6,7 +6,7 @@
   :dependencies [[org.clojure/clojure "1.10.1"]
                  [org.xerial/sqlite-jdbc "3.34.0"]
                  [org.clojure/java.jdbc "0.7.11"]
-                 [escherize/defrag "0.1.4"]
+                 [escherize/defrag "0.1.5"]
                  [mount "0.1.16"]]
   :main ^:skip-aot historia.core
   :target-path "target/%s"
